c1a58a07154b1f62379acb26b189b43c04406cee
wangmaoxiong <2268461750@qq.com>
2020-05-28 08:57:02 +0800

    格式化代码.

--- src/main/java/com/wmx/H2SmilApplication.java
+++ src/main/java/com/wmx/H2SmilApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class H2SmilApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(H2SmilApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(H2SmilApplication.class, args);
+    }
 
 }
